acefe6ebeb6b0d185aecee61135a42d2fe15d773
pratikdongre31 <pdboss358@gmail.com>
2024-05-09 00:05:11 +0530

    Second Commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const searchButton = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
-    const response = await fetch(apiUrl + city + '&appid=${apiKey}');
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
